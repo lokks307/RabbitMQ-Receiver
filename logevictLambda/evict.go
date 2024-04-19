cd5f55e1641bda87d1da2cd61152521fe076a039
Yamez <pdsfko@gmail.com>
2024-04-19 08:12:02 +0000

    webhook added

--- logevictLambda/evict.go
+++ logevictLambda/evict.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -26,6 +30,9 @@ type logFileInfo struct {
 	Path    string
 	ModTime string
 }
+type webHookMessage struct {
+	Message string `json:"text"`
+}
 
 var logFileInfos []logFileInfo
 
@@ -33,6 +40,7 @@ func main() {
 	loc, err := time.LoadLocation("Asia/Seoul")
 	if err != nil {
 		log.Printf("location error")
+		sendWebHook("location error")
 		return
 	}
 
@@ -46,25 +54,32 @@ func main() {
 
 	if thresholdVal < 0 || thresholdVal > 99 {
 		log.Print("threshold should be between 0 and 99")
+		sendWebHook("threshold should be between 0 and 99")
 		return
 	}
 	if *bucketName == "empty" {
 		log.Printf("bucketName: %s", *bucketName)
+		sendWebHook("bucketempty")
 		return
 	}
 
 	//Usage Check
 	usage, err := checkDiskUsage()
 	if err != nil {
-		fmt.Printf("Error checking disk usage: %s\n", err)
+		log.Printf("Error checking disk usage: %s\n", err)
+		sendWebHook("Error checking disk usage")
 		os.Exit(1)
 	}
 	if int(usage) < thresholdVal {
-		fmt.Printf("Disk usage is %d, below threshold %d\n", int(usage), thresholdVal)
+		l := fmt.Sprintf("Disk usage is %d, below threshold %d\n", int(usage), thresholdVal)
+		log.Printf(l)
+		sendWebHook(l)
 		return
 	}
-	fmt.Printf("Disk usage is %d, above threshold %d\n", int(usage), thresholdVal)
 
+	l := fmt.Sprintf("Disk usage is %d, above threshold %d\n", int(usage), thresholdVal)
+	log.Printf(l)
+	sendWebHook(l)
 	//add files to slice
 	err = filepath.Walk(localLogPath, fileCheck)
 
@@ -82,7 +97,10 @@ func main() {
 
 	wg.Wait()
 	endTime := time.Now().In(loc).Format("2006-01-02 15:04:05")
-	log.Printf("Eviction Job started at %s and done at %s! bye~", startTime, endTime)
+	l = fmt.Sprintf("Eviction Job started at %s and done at %s! bye~", startTime, endTime)
+	log.Println(l)
+	sendWebHook(l)
+
 	return
 }
 
@@ -144,10 +162,52 @@ func uploadFileToS3(filePath, fileName, bucket string, wg *sync.WaitGroup) error
 	}
 	defer file.Close()
 
+	upperDir := filepath.Base(filepath.Dir(filePath))
 	_, err = uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(bucket),
-		Key:    aws.String(fileName),
+		Key:    aws.String(path.Join(upperDir, fileName)),
 		Body:   file,
 	})
+
+	// err = os.Remove(filePath)
+	// if err != nil {
+	// 	log.Printf("Failed to remove file %s: %v", filePath, err)
+	// } else {
+	// 	log.Printf("File %s removed successfully", filePath)
+	// }
 	return err
+}
+
+func sendWebHook(msg string) error {
+	log.Println("sending webhook")
+	webhookURL := "https://lokksio.webhook.office.com/webhookb2/a4303448-984f-4c61-9c6d-bf574051f4c7@813bddad-fdc2-434d-b0a0-9c831c139401/IncomingWebhook/553927d201904fabbb988ec59166db31/f149ba3b-8aa6-4944-92de-6b6471a157ae"
+	message := webHookMessage{
+		Message: msg,
+	}
+	messageBytes, err := json.Marshal(message)
+	if err != nil {
+		fmt.Println("Failed to serialize message:", err)
+		return err
+	}
+
+	req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(messageBytes))
+	if err != nil {
+		fmt.Println("Failed to create HTTP request:", err)
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Failed to send HTTP request:", err)
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	return nil
 }
---
+++ logevictLambda/readme.md
@@ -0,0 +1,6 @@
+> Execution
+> \<binary> --threshold \<your threshold> --bucketname \<your bucketname>
+> Ex) ./loglambda --threshold 5 --bucketname rabbitmq-test-logbucket
+
+Currently using linux cron tab
+You may use docker or whatever you want
